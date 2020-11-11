7b2857b07658b2d9aa760c59ea44dcf8c95ad394
Beardedflea <Linzell.Godfrey@live.co.uk>
2020-11-11 14:05:47 +0000

    Added default item fields back to fleamarket.cfg

--- src/main/java/io/github/beardedflea/fleamarket/config/FleaMarketConfig.java
+++ src/main/java/io/github/beardedflea/fleamarket/config/FleaMarketConfig.java
@@ -9,10 +9,14 @@ import java.io.IOException;
 
 public class FleaMarketConfig{
 
-  //Config variables
+  //Config common variables
   boolean debugMode, joinMessage;
   int BroadcastReminder, saleInterval;
   String selectionType;
+
+  //Common defaultItemField variables
+  int defaultUptime;
+  String defaultSoldMessage, defaultBroadcast, defaultReward;
 
   private static final String[] selectionTypeArray = {"descending", "ascending", "random", "fairrandom"};
   private static final String[] selectionComments = {
@@ -33,9 +37,25 @@ public class FleaMarketConfig{
   private void loadConfig(File configDictionary) throws IOException {
     final Configuration configuration = new Configuration(new File(configDictionary, "FleaMarket.cfg"));
 
-    String category = "General";
-    String varComment = "Outputs more plugin information onto the console\n";
 
+    String category = "Common";
+
+    loadGeneralConfig("Common", configuration);
+
+    loadDefaultItemFieldsConfig("Default_Item_Fields", configuration);
+
+//    configuration.setCategoryComment("Default_Item_Fields",
+//            "If any of the listed fields are not included within the item.json, they will use these default fields listed below.");
+
+
+    if (configuration.hasChanged()) {
+      configuration.save();
+    }
+  }
+
+  private void loadGeneralConfig(String category, Configuration configuration){
+
+    String varComment = "Outputs more plugin information onto the console\n";
     this.debugMode =
             configuration.getBoolean("Debug mode",category,  false, varComment);
 
@@ -58,10 +78,61 @@ public class FleaMarketConfig{
     this.selectionType =
             configuration.getString("Reward Selection Type", category,"descending", varComment, selectionTypeArray);
 
-    if (configuration.hasChanged()) {
-      configuration.save();
-    }
   }
+
+  private void loadDefaultItemFieldsConfig(String category, Configuration configuration){
+// getString(String name, String category, String defaultValue, String comment)
+    String varComment = "placeholders: %item%, %amount%, %playerName%\n";
+    this.defaultSoldMessage =
+            configuration.getString("Default Sold Message", category,
+            "&6%playerName%, you have sold %amount% %item% to Flea Market", varComment);
+
+    varComment = "How long (in minutes) the item will be up for sale\n";
+    this.defaultUptime =
+            configuration.getInt("Default Uptime", category, 20, 0, 1000, varComment);
+
+    varComment = "#placeholders: %item%, %amount%\n";
+    this.defaultBroadcast =
+            configuration.getString("Default Broadcast", category,
+                    "&bFlea market is buying &l%amount% &r&b%item%", varComment);
+
+    varComment = "placeholders: %playerName%\n";
+    this.defaultReward =
+            configuration.getString("Default Reward", category,
+                    "/give %playerName% dirt 2", varComment);
+
+  }
+
+//  default_item_fields {
+//    # #placeholders: %item%, %amount%
+//    #  [default: &bFlea market is buying &l%amount% &r&b%item%]
+//      S:"Default Broadcast"=&bFlea market is buying &l%amount% &r&b%item%
+//
+//    # placeholders: %playerName%
+//    #  [default: /give %playerName% dirt 2]
+//      S:"Default Reward"=/give %playerName% dirt 2
+//
+//    # placeholders: %item%, %amount%, %playerName%
+//    #  [default: &6%playerName%, you have sold %amount% %item% to Flea Market]
+//      S:"Default Sold Message"=&6%playerName%, you have sold %amount% %item% to Flea Market
+//
+//    # How long (in minutes) the item will be up for sale
+//    #  [range: 0 ~ 1000, default: 20]
+//      I:"Default Uptime"=20
+//  }
+
+//  #defaultItemFields - "If any of the listed fields are not included within the item.json, they will use these default fields listed below."
+//
+//          #placeholders: %item%, %amount%, %playerName%
+//  defaultSoldMessage: "&6%playerName%, you have sold %amount% %item% to Flea Market"
+//          #How long (in minutes) the item will be up for sale; Range (min = 0, max = 1000)
+//  defaultUptime: 20
+//          #placeholders: %item%, %amount%
+//  defaultBroadcast: "&bFlea market is buying &l%amount% &r&b%item%"
+//          #placeholders: %playerName%
+//  defaultReward: "/give %playerName% dirt 2"
+
+  //getter methods
 
   public boolean debugMode(){
     return this.debugMode;
