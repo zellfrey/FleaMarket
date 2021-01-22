183ed9c5fc0c1441d28eaf76c43d32a9e04e64f3
Beardedflea <Linzell.Godfrey@live.co.uk>
2021-01-22 13:47:15 +0000

    Updated gradle properties. Version changing now only needs to be edited in 1 file

--- src/main/java/io/github/beardedflea/fleamarket/FleaMarket.java
+++ src/main/java/io/github/beardedflea/fleamarket/FleaMarket.java
@@ -30,7 +30,7 @@ public class FleaMarket
     public static final String MODID = "fleamarket";
     public static final String NAME = "Flea Market";
     public static final String MCVERSIONS = "[1.12, 1.13)";
-    public static final String VERSION = "1.2.0";
+    public static final String VERSION = "@VERSION@";
     @Mod.Instance(MODID)
     public static FleaMarket instance;
     public static FleaMarketConfig config;
